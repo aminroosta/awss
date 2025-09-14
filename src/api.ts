f94119609a9dd467ae86f786a5ab6f0464307cd0
amin roosta <amin.roosta89@gmail.com>
2025-09-14 17:30:53 -0400

    move some functions from aws.ts to api.ts

--- src/api.ts
+++ src/api.ts
@@ -3,22 +3,23 @@ import { promisify } from "util";
 import { memo } from "./util/memo";
 const execute = promisify(exec);
 
-async function shellText(command: string) {
+async function shellText(cmd: string) {
   try {
-    const { stdout, stderr } = await execute(command);
+    const { stdout, stderr } = await execute(cmd);
     return stdout.trim();
   } catch (err: any) {
-    err.command = command;
+    err.command = cmd;
     throw err;
   }
 }
 
-async function shell<R>(command: string) {
+async function shell<R>(cmd: string, no_suffix = false) {
   try {
-    const { stdout, stderr } = await execute(command);
+    const args = no_suffix ? cmd : `${cmd} --output json`;
+    const { stdout, stderr } = await execute(args);
     return JSON.parse(stdout) as R;
   } catch (err: any) {
-    err.command = command;
+    err.command = cmd;
     throw err;
   }
 }
@@ -27,6 +28,28 @@ export const awsRegion = memo(() => shellText("aws configure get region"));
 
 export const awsCallerIdentity = memo(() =>
   shell<{ UserId: string; Account: string; Arn: string }>(
-    "aws sts get-caller-identity --output json",
+    "aws sts get-caller-identity",
   ),
 );
+
+export const awsCliVersion = memo(async () => {
+  const version = await shellText("aws --version");
+  return /aws-cli\/([^\s]+)/.exec(version)![1]!;
+});
+
+export const awsListBuckets = memo(async () => {
+  type R = { Buckets: { Name: string; CreationDate: string }[]; Owner: { DisplayName: string; ID: string } };
+  const result = await shell<R>("aws s3api list-buckets");
+  const maxLen = Math.max(...result.Buckets.map((b) => b.Name.length));
+  for (let bucket of result.Buckets) {
+    bucket.Name = bucket.Name.padEnd(maxLen, " ");
+    bucket.CreationDate = bucket.CreationDate.split("T")[0]!;
+  }
+  return result;
+});
+
+export const awsListStacks = memo(async () => {
+  type R = { StackSummaries: { StackName: string; StackStatus: string; CreationTime: string; StackId: string }[] };
+  const result = await shell<R>("aws cloudformation list-stacks");
+  return result.StackSummaries.sort((a, b) => a.StackStatus.localeCompare(b.StackStatus));
+});
--- src/aws.ts
+++ src/aws.ts
@@ -5,99 +5,6 @@ import { memo } from "./util/memo";
 export * from "./api";
 import { awsRegion } from "./api";
 
-export const awsUrls: Record<string, (id: string) => Promise<string>> = {
-  vpc: async (id) => {
-    const region = await awsRegion();
-    return `https://console.aws.amazon.com/vpcconsole/home?region=${region}#VpcDetails:VpcId=${id}`;
-  },
-  securitygroup: async (id) => {
-    const region = await awsRegion();
-    return `https://console.aws.amazon.com/ec2/home?region=${region}#SecurityGroup:groupId=${id}`;
-  },
-  instances: async (id) => {
-    const region = await awsRegion();
-    return `https://${region}.console.aws.amazon.com/ec2/home?region=${region}#InstanceDetails:instanceId=${id}`;
-  },
-  users: async (id) => {
-    const region = await awsRegion();
-    return `https://console.aws.amazon.com/iam/home?region=${region}#/users/${id}`;
-  },
-  stacks: async (id) => {
-    const region = await awsRegion();
-    return `https://console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/stackinfo?stackId=${encodeURIComponent(id)}`;
-  },
-  subnets: async (id) => {
-    const region = await awsRegion();
-    return `https://console.aws.amazon.com/vpcconsole/home?region=${region}#SubnetDetails:subnetId=${id}`;
-  },
-  buckets: async (id) => `https://console.aws.amazon.com/s3/buckets/${id}`,
-  stackparameters: async (id) => {
-    const region = await awsRegion();
-    return `https://console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/parameters?stackId=${encodeURIComponent(id)}&tabId=parameters`;
-  },
-  stackevents: async (id) => {
-    const region = await awsRegion();
-    return `https://console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/events?stackId=${encodeURIComponent(id)}&tabId=events`;
-  },
-  resources: async (id) => {
-    const region = await awsRegion();
-    return `https://console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/resources?stackId=${encodeURIComponent(id)}&tabId=resources`;
-  },
-  repositories: async (id) =>
-    `https://console.aws.amazon.com/ecr/repositories/${id}`,
-  clusters: async (arn) => {
-    const region = await awsRegion();
-    return `https://console.aws.amazon.com/ecs/v2/clusters/${encodeURIComponent(arn.split("/").pop() || arn)}/services?region=${region}`;
-  },
-};
-
-export const awsCliVersion = memo(async () => {
-  let version = "";
-  try {
-    version = await $`aws --version`.text();
-  } catch (e: any) {
-    e.command = "aws --version";
-    throw e;
-  }
-  return /aws-cli\/([^\s]+)/.exec(version)![1]!;
-});
-
-export const awsListBuckets = memo(async () => {
-  try {
-    let result = (await $`aws s3api list-buckets --output json`.json()) as {
-      Buckets: { Name: string; CreationDate: string }[];
-      Owner: { DisplayName: string; ID: string };
-    };
-    const nameLen = Math.max(...result.Buckets.map((b) => b.Name.length));
-    for (let bucket of result.Buckets) {
-      bucket.Name = bucket.Name.padEnd(nameLen, " ");
-      bucket.CreationDate = bucket.CreationDate.split("T")[0]!;
-    }
-    return result;
-  } catch (e: any) {
-    e.command = "aws s3api list-buckets --output json";
-    throw e;
-  }
-});
-
-export const awsListStacks = memo(async () => {
-  try {
-    const result =
-      (await $`aws cloudformation list-stacks --output json`.json()) as {
-        StackSummaries: {
-          StackId: string;
-          StackName: string;
-          CreationTime: string;
-          StackStatus: string;
-        }[];
-      };
-    return result.StackSummaries;
-  } catch (e: any) {
-    e.command = "aws cloudformation list-stacks --output json";
-    throw e;
-  }
-});
-
 export const awsListObjectsV2 = memo(
   async (bucket: string, prefix: string, delimiter = "/") => {
     try {
@@ -867,3 +774,49 @@ export const awsEcsListTasks = memo(
   },
   30_000,
 );
+
+export const awsUrls: Record<string, (id: string) => Promise<string>> = {
+  vpc: async (id) => {
+    const region = await awsRegion();
+    return `https://console.aws.amazon.com/vpcconsole/home?region=${region}#VpcDetails:VpcId=${id}`;
+  },
+  securitygroup: async (id) => {
+    const region = await awsRegion();
+    return `https://console.aws.amazon.com/ec2/home?region=${region}#SecurityGroup:groupId=${id}`;
+  },
+  instances: async (id) => {
+    const region = await awsRegion();
+    return `https://${region}.console.aws.amazon.com/ec2/home?region=${region}#InstanceDetails:instanceId=${id}`;
+  },
+  users: async (id) => {
+    const region = await awsRegion();
+    return `https://console.aws.amazon.com/iam/home?region=${region}#/users/${id}`;
+  },
+  stacks: async (id) => {
+    const region = await awsRegion();
+    return `https://console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/stackinfo?stackId=${encodeURIComponent(id)}`;
+  },
+  subnets: async (id) => {
+    const region = await awsRegion();
+    return `https://console.aws.amazon.com/vpcconsole/home?region=${region}#SubnetDetails:subnetId=${id}`;
+  },
+  buckets: async (id) => `https://console.aws.amazon.com/s3/buckets/${id}`,
+  stackparameters: async (id) => {
+    const region = await awsRegion();
+    return `https://console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/parameters?stackId=${encodeURIComponent(id)}&tabId=parameters`;
+  },
+  stackevents: async (id) => {
+    const region = await awsRegion();
+    return `https://console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/events?stackId=${encodeURIComponent(id)}&tabId=events`;
+  },
+  resources: async (id) => {
+    const region = await awsRegion();
+    return `https://console.aws.amazon.com/cloudformation/home?region=${region}#/stacks/resources?stackId=${encodeURIComponent(id)}&tabId=resources`;
+  },
+  repositories: async (id) =>
+    `https://console.aws.amazon.com/ecr/repositories/${id}`,
+  clusters: async (arn) => {
+    const region = await awsRegion();
+    return `https://console.aws.amazon.com/ecs/v2/clusters/${encodeURIComponent(arn.split("/").pop() || arn)}/services?region=${region}`;
+  },
+};
