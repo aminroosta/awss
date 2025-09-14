f6fc0c2049cd9fd5dcf378a3d8aa7d76fdefb7c4
amin roosta <amin.roosta89@gmail.com>
2025-09-14 19:22:39 -0400

    describe instances

--- src/api/ec2.ts
+++ src/api/ec2.ts
@@ -78,3 +78,91 @@ export const awsEc2DescribeSubnet = async (subnetId: string) => {
 
 export const awsEc2DescribeSubnetYaml = (subnetId: string) =>
   aws(`aws ec2 describe-subnets --subnet-ids='${subnetId}'`, "yaml");
+
+export const awsEc2DescribeInstances = async () => {
+  type DescribeInstances = {
+    Reservations: {
+      ReservationId: string;
+      OwnerId: string;
+      RequesterId: string;
+      Groups: { GroupName: string; GroupId: string }[];
+      Instances: {
+        InstanceId: string;
+        ImageId: string;
+        State: { Code: number; Name: string };
+        PrivateDnsName?: string;
+        PublicDnsName?: string;
+        InstanceType: string;
+        LaunchTime: string;
+        Placement: {
+          AvailabilityZone: string;
+          GroupName: string;
+          Tenancy: string;
+        };
+        Monitoring: { State: string };
+        SubnetId?: string;
+        VpcId?: string;
+        PrivateIpAddress?: string;
+        PublicIpAddress?: string;
+        Architecture: string;
+        RootDeviceType: string;
+        RootDeviceName: string;
+        Hypervisor: string;
+        EnaSupport: boolean;
+        EbsOptimized: boolean;
+        ClientToken?: string;
+        BlockDeviceMappings?: {
+          DeviceName: string;
+          Ebs: {
+            VolumeId: string;
+            Status: string;
+            AttachTime: string;
+            DeleteOnTermination: boolean;
+          };
+        }[];
+        NetworkInterfaces?: {
+          Association?: {
+            IpOwnerId?: string;
+            PublicDnsName?: string;
+            PublicIp?: string;
+          };
+          Attachment?: {
+            AttachTime: string;
+            AttachmentId: string;
+            DeleteOnTermination: boolean;
+            DeviceIndex: number;
+            Status: string;
+            NetworkCardIndex?: number;
+          };
+          Description?: string;
+          Groups: { GroupId: string; GroupName: string }[];
+          Ipv6Addresses?: any[];
+          MacAddress: string;
+          NetworkInterfaceId: string;
+          OwnerId: string;
+          PrivateDnsName: string;
+          PrivateIpAddress: string;
+          PrivateIpAddresses: {
+            Association?: {
+              IpOwnerId?: string;
+              PublicDnsName?: string;
+              PublicIp?: string;
+            };
+            Primary: boolean;
+            PrivateDnsName: string;
+            PrivateIpAddress: string;
+          }[];
+          SourceDestCheck: boolean;
+          Status: string;
+          SubnetId: string;
+          VpcId: string;
+          InterfaceType: string;
+          Operator?: { Managed: boolean };
+        }[];
+        Tags?: { Key: string; Value: string }[];
+      }[];
+    }[];
+  };
+  const result = await aws<DescribeInstances>(`aws ec2 describe-instances`);
+  return result.Reservations.flatMap((r) => r.Instances);
+};
